170a472ae99d41653730e3fa6491ab5644b68c9d
Magician-codeer <creatorkenbeast@gmail.com>
2024-07-06 14:17:35 +0300

    Update config.js

--- config.js
+++ config.js
@@ -76,8 +76,8 @@ module.exports = {
  
   author : process.env.PACK_AUTHER|| "",
   packname: process.env.PACK_NAME || "",
-  botname : process.env.BOT_NAME  || "Kenbeast",
-  ownername:process.env.OWNER_"Kenbeast",
+  botname : process.env.BOT_NAME  || "Suhail_MD",
+  ownername:process.env.OWNER_"Suhail_MD,
 
 
   errorChat : process.env.ERROR_CHAT || "",
